a5b8303fd91eb9e4ec2d6610f55cb340fa951469
AnhNH <anhnh11032001@gmail.com>
2025-07-07 11:33:01 +0700

    prompts

--- src/main/java/com/ic/spring_ai_demo/service/ChatService.java
+++ src/main/java/com/ic/spring_ai_demo/service/ChatService.java
@@ -1,6 +1,9 @@
 package com.ic.spring_ai_demo.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 import com.ic.spring_ai_demo.dto.ChatRequest;
@@ -15,8 +18,17 @@ public class ChatService {
     }
 
     public String chat(ChatRequest request) {
+        SystemMessage systemMessage = new SystemMessage("""
+                You are Jae AI
+                You should respond to the user's request in a friendly and helpful manner.
+                """);
+
+        UserMessage userMessage = new UserMessage(request.message());
+
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
         return chatClient
-                .prompt(request.message())
+                .prompt(prompt)
                 .call()
                 .content();
     }
