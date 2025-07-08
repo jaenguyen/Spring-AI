2ef57b043ed74c33211ff805668eb55cdf91c183
AnhNH <anhnh11032001@gmail.com>
2025-07-08 14:09:06 +0700

    process image

--- src/main/java/com/ic/spring_ai_demo/controller/ChatController.java
+++ src/main/java/com/ic/spring_ai_demo/controller/ChatController.java
@@ -3,7 +3,9 @@ package com.ic.spring_ai_demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ic.spring_ai_demo.dto.ChatRequest;
 import com.ic.spring_ai_demo.service.ChatService;
@@ -13,10 +15,17 @@ public class ChatController {
 
     @Autowired
     ChatService chatService;
-    
+
     @PostMapping("/chat")
     String chat(@RequestBody ChatRequest request) {
-        
+
         return chatService.chat(request);
     }
+
+    @PostMapping("/chat-with-image")
+    public String chatWithImage(@RequestParam MultipartFile file, @RequestParam String message) {
+
+        return chatService.chatWithImage(file, message);
+    }
+
 }
--- src/main/java/com/ic/spring_ai_demo/service/ChatService.java
+++ src/main/java/com/ic/spring_ai_demo/service/ChatService.java
@@ -3,8 +3,12 @@ package com.ic.spring_ai_demo.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ic.spring_ai_demo.dto.ChatRequest;
 
@@ -29,6 +33,27 @@ public class ChatService {
 
         return chatClient
                 .prompt(prompt)
+                .call()
+                .content();
+    }
+
+    public String chatWithImage(MultipartFile file, String message) {
+        Media media = Media.builder()
+                .mimeType(MimeTypeUtils.parseMimeType(file.getContentType()))
+                .data(file.getResource())
+                .build();
+
+        
+        ChatOptions chatOptions = ChatOptions.builder()
+                .temperature(0D)
+                .build();
+
+        return chatClient.prompt()
+                .options(chatOptions)
+                .system("You are Jae AI. You should respond to the user's request in a friendly and helpful manner.")
+                .user(promptUserSpec -> promptUserSpec
+                        .media(media)
+                        .text(message))
                 .call()
                 .content();
     }
